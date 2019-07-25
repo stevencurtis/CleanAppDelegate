bf5d4bdd19e29dd3f98132a9a537c549628fd6bc
Steven Curtis <stevenpcurtis@hotmail.com>
2019-07-25 08:59:50 +0800

    Initial Commit

---
+++ AppServicesSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppServicesSwift
+//
+//  Created by Steven Curtis on 25/07/2019.
+//  Copyright © 2019 Steven Curtis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
